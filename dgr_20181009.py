d2675f91f7553fa0907a199041530e0e288d89ea
user <bebrisha12@inbox.lv>
2018-10-09 14:03:18 +0300

    20181009_14_03

---
+++ dgr_20181009.py
@@ -0,0 +1,56 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Equals 5
+Greater than 4
+Great then or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Before 5
+ls 5
+ls Still 5
+Third 5
+Afterwards 5
+Before 6
+Afterwards 6
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Before 5
+ls 5
+ls Still 5
+Third 5
+Afterwards 5
+Before 6
+Afterwards 6
+('First', -1)
+('Second', 123)
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+('First', -1)
+('Second', 123)
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Hello
+('Done', -1)
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Enter a number:42
+Not a number
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Enter a number:42
+Nice work
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Ievadi x:7
+Below 10
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Ievadi x:15
+Below 20
+>>> 
---
+++ test_1.py
@@ -0,0 +1,10 @@
+x= input ('Ievadi x:')
+if x<2:
+    print ('Below 2')
+elif x<10:
+    print('Below 10')
+elif x< 20:
+    print('Below 20')
+else:
+    print('Something else')
+    
